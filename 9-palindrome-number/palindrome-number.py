07f9c2af66f6ab3b118f619187b55b34ed1aa11e
suhail2oo5 <gamersuhail21@gmail.com>
2026-07-19 11:56:49 +0530

    Time: 8 ms (71.52%) | Memory: 12.2 MB (89.61%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        a = str(x)
+        b = a[::-1]
+        if a == b:
+            return True
+        else:
+            return False
